f3eb3881b81e3b677438a9101fd06de8f302f223
AshishMishra9511 <ashish.mishra951127@gmail.com>
2019-02-13 13:19:29 +0530

    referenced code from demo ble-plx and created BleModule

--- src/containers/MainConatainer/MainContainer.js
+++ src/containers/MainConatainer/MainContainer.js
@@ -8,39 +8,17 @@ export default class MainContainer extends Component {
         super();
         this.manager = new BleManager();
         this.state = {
-            info: "", 
+            info: "",
             values: {},
-            devices : [],
+            devices: [],
             deviceIds: [],
-            error : null,
-            isScanning:false,
+            error: null,
+            isScanning: false,
         }
-        this.prefixUUID = "f000aa"
-        this.suffixUUID = "-0451-4000-b000-000000000000"
-        this.sensors = {
-            0: "Temperature",
-            1: "Accelerometer",
-            2: "Humidity",
-            3: "Magnetometer",
-            4: "Barometer",
-            5: "Gyroscope"
-        }
-    }
-
-    serviceUUID(num) {
-        return this.prefixUUID + num + "0" + this.suffixUUID
-    }
-
-    notifyUUID(num) {
-        return this.prefixUUID + num + "1" + this.suffixUUID
-    }
-
-    writeUUID(num) {
-        return this.prefixUUID + num + "2" + this.suffixUUID
     }
 
     scanForDevices() {
-        this.setState({isScanning:!this.state.isScanning});
+        this.setState({ isScanning: !this.state.isScanning });
         this.manager.startDeviceScan(null, null, (error, device) => {
             if (error) {
                 console.log(error);
@@ -48,14 +26,14 @@ export default class MainContainer extends Component {
                 return
             }
             else {
-                this.getDevicesFromIds([...this.state.deviceIds,device.id]);
+                this.getDevicesFromIds([...this.state.deviceIds, device.id]);
             }
         });
-        setTimeout(this.stopScanning,10000);
+        setTimeout(this.stopScanning, 10000);
     }
 
     stopScanning = () => {
-        this.setState({isScanning:!this.state.isScanning});
+        this.setState({ isScanning: !this.state.isScanning });
         this.manager.stopDeviceScan();
         console.log("device scan finished");
     }
@@ -74,47 +52,50 @@ export default class MainContainer extends Component {
         this.scanForDevices();
     }
 
-    getDevicesFromIds = (Ids) =>{
+    getDevicesFromIds = (Ids) => {
         let antiDuplicateIds = Array.from(new Set(Ids))
         let devices = this.manager.devices(antiDuplicateIds)
         devices.then(data => {
-            this.setState({devices:data,deviceIds:antiDuplicateIds});
+            this.setState({ devices: data, deviceIds: antiDuplicateIds });
         });
     }
 
-    connectToDevice = device =>{
+    connectToDevice = device => {
+        this.stopScanning();
+        console.log("stopped scnannig");
         device.connect()
-        .then(device=>{
-            console.log("connected")
-        })
-        .catch(error =>{
-            console.log(error)
-        });
+            .then((device) => {
+                console.log("Discovering services and characteristics");
+                return device.discoverAllServicesAndCharacteristics();
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
- 
+
     render() {
-        console.log("devices datataatatat",this.state.devices);
-        let deviceIds = this.state.devices.map((element,index)=>{
+        console.log("devices datataatatat", this.state.devices);
+        let deviceIds = this.state.devices.map((element, index) => {
             return (
-                <TouchableOpacity 
-                onPress={() => this.connectToDevice(element)}
-                style={{ backgroundColor: "#888", height: 50, display: "flex", justifyContent: "center"}}
+                <TouchableOpacity
+                    onPress={() => this.connectToDevice(element)}
+                    style={{ backgroundColor: "#888", height: 50, display: "flex", justifyContent: "center" }}
                 >
-                <Text>{index}.   {element.name?element.name:element.id}</Text>
+                    <Text>{index}.   {element.name ? element.name : element.id}</Text>
                 </TouchableOpacity>
             )
         })
         return (
             <View>
                 <TouchableOpacity style={{ backgroundColor: "#000", height: 200, display: "flex", justifyContent: "center", alignItems: "center" }} onPress={this.onPressHandler}>
-                {this.state.isScanning ? 
-                    (<Text style={{ color: "#fff" }}>Scanning</Text>)
-                    :
-                    (<Text style={{ color: "#fff" }}>Press Here</Text>)}
+                    {this.state.isScanning ?
+                        (<Text style={{ color: "#fff" }}>Scanning</Text>)
+                        :
+                        (<Text style={{ color: "#fff" }}>Press Here</Text>)}
                 </TouchableOpacity>
                 {deviceIds}
             </View>
-            
+
         )
     }
 }
